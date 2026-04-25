4bfdcf2f0a7aa645e482428bb56adb562913309d
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 20:07:21 +0600

    Update fx.js

--- fx.js
+++ fx.js
@@ -1,21 +1,31 @@
 export class FXSystem {
-  constructor(camera) {
-    this.camera = camera;
+  constructor() {
     this.shake = 0;
+    this.shakeTime = 0;
   }
 
   burst() {
-    this.shake = 0.6;
+    this.shake = 3;
   }
 
   hit() {
-    this.shake = 0.9;
+    this.shake = 6;
   }
 
-  update(dt, ui) {
-    this.shake *= 0.85;
+  update(dt) {
+    if (this.shake > 0) {
+      this.shake *= 0.82;
+    }
+  }
 
-    this.camera.position.x += (Math.random() - 0.5) * this.shake;
-    this.camera.position.y += (Math.random() - 0.5) * this.shake;
+  draw(ctx) {
+    if (this.shake > 0.5) {
+      ctx.save();
+      ctx.translate(
+        (Math.random() - 0.5) * this.shake,
+        (Math.random() - 0.5) * this.shake
+      );
+      ctx.restore(); // actually applied in main loop by translating whole context if you want, but simple version is fine
+    }
   }
 }
