12c326d86c0f01f268404fa8914cb47150640048
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 13:26:21 +0600

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,11 +1,11 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
-
 export class AudioEngine {
-  init(camera) {
-    this.listener = new THREE.AudioListener();
-    camera.add(this.listener);
+  init(camera) {}
+
+  playShot() {
+    console.log("SHOT");
   }
 
-  playShot() {}
-  playHit() {}
+  playHit() {
+    console.log("HIT");
+  }
 }
