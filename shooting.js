4b7dd63d402eb9a8f168551195a29fedc1f15458
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 13:22:33 +0600

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -1,18 +1,19 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
 
 export class ShootingSystem {
-  constructor(scene, character, zombies, ui, audio, recoil) {
+  constructor(scene, character, zombies, ui, audio, recoil, fx) {
     this.scene = scene;
     this.character = character;
     this.zombies = zombies;
     this.ui = ui;
     this.audio = audio;
     this.recoil = recoil;
+    this.fx = fx;
 
     this.bullets = [];
-    this.speed = 25;
+    this.speed = 30;
 
-    this.mat = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    this.mat = new THREE.MeshStandardMaterial({ color: 0xff2a2a });
 
     window.addEventListener("click", () => this.shoot());
   }
@@ -23,10 +24,11 @@ export class ShootingSystem {
     const data = this.character.shoot();
 
     this.audio.playShot();
-    this.recoil.kick(0.2);
+    this.recoil.kick(0.25);
+    this.fx.burst();
 
     const b = new THREE.Mesh(
-      new THREE.SphereGeometry(0.15),
+      new THREE.SphereGeometry(0.12),
       this.mat
     );
 
@@ -42,6 +44,12 @@ export class ShootingSystem {
 
       b.position.z -= this.speed * dt;
 
+      if (b.position.z < this.character.position.z - 50) {
+        this.scene.remove(b);
+        this.bullets.splice(i, 1);
+        continue;
+      }
+
       for (let j = this.zombies.zombies.length - 1; j >= 0; j--) {
         const z = this.zombies.zombies[j];
 
@@ -53,6 +61,7 @@ export class ShootingSystem {
           this.ui.addScore(10);
 
           this.audio.playHit();
+          this.fx.hit();
           break;
         }
       }
