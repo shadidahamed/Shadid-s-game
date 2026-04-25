2ce7d558d8cee934bc52bbeece46f41dbe1ffdea
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 12:34:56 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,28 +1,15 @@
 export class UI {
-  
   constructor() {
     this.score = 0;
     this.health = 100;
     this.gameOver = false;
 
-    this.el = document.createElement('div');
-    this.el.style.position = 'absolute';
-    this.el.style.top = '10px';
-    this.el.style.right = '10px';
-    this.el.style.color = 'red';
+    this.el = document.createElement("div");
+    this.el.style.position = "absolute";
+    this.el.style.top = "10px";
+    this.el.style.right = "10px";
+    this.el.style.color = "red";
     document.body.appendChild(this.el);
-
-    this.gameOverScreen = document.createElement('div');
-    this.gameOverScreen.innerText = 'GAME OVER';
-    this.gameOverScreen.style.display = 'none';
-    this.gameOverScreen.style.position = 'absolute';
-    this.gameOverScreen.style.top = '50%';
-    this.gameOverScreen.style.left = '50%';
-    this.gameOverScreen.style.transform = 'translate(-50%, -50%)';
-    this.gameOverScreen.style.fontSize = '40px';
-    this.gameOverScreen.style.color = 'red';
-
-    document.body.appendChild(this.gameOverScreen);
 
     this.render();
   }
@@ -37,13 +24,27 @@ export class UI {
     if (this.gameOver) return;
 
     this.health -= v;
+
     if (this.health <= 0) {
       this.health = 0;
       this.gameOver = true;
-      this.gameOverScreen.style.display = 'block';
+      this._gameOver();
     }
 
     this.render();
+  }
+
+  _gameOver() {
+    const g = document.createElement("div");
+    g.innerText = "GAME OVER";
+    g.style.position = "absolute";
+    g.style.top = "50%";
+    g.style.left = "50%";
+    g.style.transform = "translate(-50%,-50%)";
+    g.style.color = "red";
+    g.style.fontSize = "50px";
+
+    document.body.appendChild(g);
   }
 
   render() {
