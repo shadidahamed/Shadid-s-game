590d39314142b3fd38a92e127473a65b045bd91f
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 20:55:46 +0600

    Update fx.js

--- fx.js
+++ fx.js
@@ -1,22 +1,5 @@
 export class FXSystem {
-  constructor() {
-    this.shake = 0;
-  }
-
-  burst(amount = 3) {
-    this.shake = Math.max(this.shake, amount);
-  }
-
-  hit() {
-    this.shake = Math.max(this.shake, 7);
-  }
-
-  update(dt) {
-    this.shake *= 0.84;
-    if (this.shake < 0.2) this.shake = 0;
-  }
-
-  draw(ctx) {
-    // Shake is applied in main.js loop
-  }
+  constructor() { this.shake = 0; }
+  burst(v = 4) { this.shake = Math.max(this.shake, v); }
+  update(dt) { this.shake *= 0.85; }
 }
