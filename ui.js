e42080313ec4ddc918e1f33eb8653ae9b00e9b1e
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 13:23:49 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,14 +1,10 @@
-
 export class UI {
   constructor() {
     this.score = 0;
     this.health = 100;
     this.gameOver = false;
 
-    this.el = document.createElement("div");
-    this.el.style.color = "red";
-    document.body.appendChild(this.el);
-
+    this.el = document.getElementById("hud");
     this.render();
   }
 
@@ -21,25 +17,18 @@ export class UI {
     this.health -= v;
 
     if (this.health <= 0) {
-      this.gameOver = true;
       this.health = 0;
+      this.gameOver = true;
 
-      const g = document.createElement("div");
-      g.innerText = "SYSTEM FAILURE";
-      g.style.position = "absolute";
-      g.style.top = "50%";
-      g.style.left = "50%";
-      g.style.transform = "translate(-50%,-50%)";
-      g.style.color = "red";
-      g.style.fontSize = "40px";
-
-      document.body.appendChild(g);
+      this.el.innerHTML = "SYSTEM FAILURE // ENTITY LOST";
     }
 
     this.render();
   }
 
   render() {
-    this.el.innerHTML = `SCORE: ${this.score} | HP: ${this.health}`;
+    if (!this.gameOver) {
+      this.el.innerHTML = `SCORE:${this.score} | HP:${this.health}`;
+    }
   }
 }
