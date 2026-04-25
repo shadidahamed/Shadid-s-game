99d86d60110c8035d5ecdfc3b410d3a886a3469b
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 12:24:33 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,26 +1,51 @@
 export class UI {
   constructor() {
     this.score = 0;
+    this.health = 100;
+    this.gameOver = false;
 
-    this.scoreElement = document.createElement('div');
-    this.scoreElement.style.position = 'absolute';
-    this.scoreElement.style.top = '10px';
-    this.scoreElement.style.right = '10px';
-    this.scoreElement.style.color = 'red';
-    this.scoreElement.style.fontSize = '20px';
-    this.scoreElement.style.textShadow = '0 0 10px red';
+    this.el = document.createElement('div');
+    this.el.style.position = 'absolute';
+    this.el.style.top = '10px';
+    this.el.style.right = '10px';
+    this.el.style.color = 'red';
+    document.body.appendChild(this.el);
 
-    document.body.appendChild(this.scoreElement);
+    this.gameOverScreen = document.createElement('div');
+    this.gameOverScreen.innerText = 'GAME OVER';
+    this.gameOverScreen.style.display = 'none';
+    this.gameOverScreen.style.position = 'absolute';
+    this.gameOverScreen.style.top = '50%';
+    this.gameOverScreen.style.left = '50%';
+    this.gameOverScreen.style.transform = 'translate(-50%, -50%)';
+    this.gameOverScreen.style.fontSize = '40px';
+    this.gameOverScreen.style.color = 'red';
 
-    this.update();
+    document.body.appendChild(this.gameOverScreen);
+
+    this.render();
   }
 
-  addScore(value) {
-    this.score += value;
-    this.update();
+  addScore(v) {
+    if (this.gameOver) return;
+    this.score += v;
+    this.render();
   }
 
-  update() {
-    this.scoreElement.innerText = `SCORE: ${this.score}`;
+  damage(v) {
+    if (this.gameOver) return;
+
+    this.health -= v;
+    if (this.health <= 0) {
+      this.health = 0;
+      this.gameOver = true;
+      this.gameOverScreen.style.display = 'block';
+    }
+
+    this.render();
+  }
+
+  render() {
+    this.el.innerHTML = `SCORE: ${this.score} | HP: ${this.health}`;
   }
 }
