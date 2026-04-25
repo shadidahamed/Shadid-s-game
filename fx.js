72bb646a35061832b8929a4a6eda8aa685e2d8ba
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 13:24:24 +0600

    Update fx.js

--- fx.js
+++ fx.js
@@ -1,24 +1,21 @@
 export class FXSystem {
-  constructor(scene, camera) {
+  constructor(camera) {
     this.camera = camera;
     this.shake = 0;
-
-    this.overlay = document.createElement("div");
-    this.overlay.style.position = "absolute";
-    this.overlay.style.width = "100%";
-    this.overlay.style.height = "100%";
-    this.overlay.style.background = "radial-gradient(circle, transparent 25%, black)";
-    this.overlay.style.opacity = "0.6";
-
-    document.body.appendChild(this.overlay);
   }
 
-  update(dt, character, ui) {
-    this.shake *= 0.9;
+  burst() {
+    this.shake = 0.6;
+  }
+
+  hit() {
+    this.shake = 0.9;
+  }
+
+  update(dt, ui) {
+    this.shake *= 0.85;
 
     this.camera.position.x += (Math.random() - 0.5) * this.shake;
     this.camera.position.y += (Math.random() - 0.5) * this.shake;
-
-    this.overlay.style.opacity = 0.3 + (1 - ui.health / 100) * 0.6;
   }
 }
