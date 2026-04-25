96d58ed37cae301ea382ffd1808eccdeb4b177df
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 20:14:36 +0600

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -1,10 +1,11 @@
 export class ShootingSystem {
-  constructor(character, zombies, ui, audio, fx) {
+  constructor(character, zombies, ui, audio, fx, particles) {
     this.character = character;
     this.zombies = zombies;
     this.ui = ui;
     this.audio = audio;
     this.fx = fx;
+    this.particles = particles;
     this.bullets = [];
   }
 
@@ -13,14 +14,16 @@ export class ShootingSystem {
     if (!data) return;
 
     this.audio.playShot();
-    this.fx.burst();
+    this.fx.burst(4);
+
+    this.particles.muzzleFlash(this.character.x, this.character.y - 45, data.level);
 
     this.bullets.push({
       x: data.x,
       y: data.y,
       speed: data.speed,
       power: data.power,
-      radius: 6
+      radius: 7 + data.level * 1.5
     });
   }
 
@@ -29,25 +32,19 @@ export class ShootingSystem {
       const b = this.bullets[i];
       b.y -= b.speed * dt;
 
-      if (b.y < -20) {
+      if (b.y < -30) {
         this.bullets.splice(i, 1);
         continue;
       }
 
       for (let j = this.zombies.zombies.length - 1; j >= 0; j--) {
         const z = this.zombies.zombies[j];
-        if (Math.hypot(b.x - z.x, b.y - z.y) < 28) {
-          z.health -= b.power;
+        if (Math.hypot(b.x - z.x, b.y - z.y) < 30) {
+          this.zombies.hit(j, b.power);
+          this.particles.sparkBurst(b.x, b.y);
           this.bullets.splice(i, 1);
-
-          if (z.health <= 0) {
-            this.zombies.remove(j);
-            this.ui.addScore(20 + Math.floor(this.zombies.director?.wave || 0) * 2);
-            this.audio.playHit();
-            this.fx.hit();
-          } else {
-            this.audio.playHit(); // light hit sound
-          }
+          this.audio.playHit();
+          this.fx.hit();
           break;
         }
       }
@@ -55,16 +52,20 @@ export class ShootingSystem {
   }
 
   draw(ctx) {
+    ctx.shadowBlur = 12;
+    ctx.shadowColor = '#ff5555';
     ctx.fillStyle = '#ff2a2a';
+
     for (const b of this.bullets) {
       ctx.beginPath();
       ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
       ctx.fill();
 
-      // trail
-      ctx.globalAlpha = 0.4;
-      ctx.fillRect(b.x - 3, b.y + 8, 6, 18);
+      // Trail
+      ctx.globalAlpha = 0.5;
+      ctx.fillRect(b.x - 4, b.y + 10, 8, 25);
       ctx.globalAlpha = 1;
     }
+    ctx.shadowBlur = 0;
   }
 }
