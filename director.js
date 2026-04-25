2befb2542e36abd8fa1ae0e91b200cfecd6962ed
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-04-25 20:07:48 +0600

    Update director.js

--- director.js
+++ director.js
@@ -1,11 +1,22 @@
 export class Director {
+  constructor() {
+    this.wave = 0;
+    this.timer = 0;
+  }
+
   spawnRate() {
-    return 1.2;
+    return Math.max(0.35, 1.1 - this.wave * 0.04);
   }
 
   zombieSpeed() {
-    return 6;
+    return 220 + this.wave * 18;
   }
 
-  update(dt) {}
+  update(dt) {
+    this.timer += dt;
+    if (this.timer > 12) {
+      this.wave++;
+      this.timer = 0;
+    }
+  }
 }
